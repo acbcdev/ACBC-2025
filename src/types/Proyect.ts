38364271f5dbc2094e26670760b0bc2029c934d4
acbc <acbc.dev@gmail.com>
2025-05-07 11:22:21 -0500

    refactor: standardize formatting in project type definitions and improve code readability

--- src/types/Proyect.ts
+++ src/types/Proyect.ts
@@ -1,12 +1,12 @@
 export type ProyectType = {
-	link: string;
-	github: string;
-	img: string;
-	name: string;
-	tags: string[];
-	description: string;
-	iconTecnology?: string;
-	favicon?: string;
-	classNames: string;
-	color?: string;
+  link: string;
+  github: string;
+  img: string;
+  name: string;
+  tags: string[];
+  description: string;
+  iconTecnology?: string;
+  favicon?: string;
+  classNames: string;
+  color?: string;
 };
